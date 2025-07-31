bd79ffcc3791350a3b578fb710332ccd95473947
Adham Elagaty <adhamalagaty@gmail.com>
2025-08-01 01:14:44 +0300

    add CurvedListWheelTextItem widget for styled and animated text based on item state

---
+++ lib/src/widgets/curved_list_wheel_text_item.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import '../models/curved_list_item_state.dart';
+import '../models/curved_list_wheel_text_style.dart';
+
+/// A helper widget for easily displaying styled text within a [CurvedListWheel].
+///
+/// It uses a [CurvedListWheelTextStyle] object to automatically apply different
+/// styles based on whether the item is selected, or its distance from center.
+class CurvedListWheelTextItem extends StatelessWidget {
+  /// The text to display.
+  final String text;
+
+  /// The state of the item, provided by the [CurvedListWheel.itemBuilder].
+  final CurvedListItemState itemState;
+
+  /// The styling configuration for the text.
+  final CurvedListWheelTextStyle style;
+
+  const CurvedListWheelTextItem({
+    super.key,
+    required this.text,
+    required this.itemState,
+    required this.style,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle effectiveStyle = style.unselectedStyle;
+
+    if (itemState.isSelected) {
+      effectiveStyle = style.selectedStyle;
+    } else if (style.distanceSpecificStyles.containsKey(itemState.distance)) {
+      effectiveStyle = style.distanceSpecificStyles[itemState.distance]!;
+    }
+    
+    return AnimatedDefaultTextStyle(
+      duration: const Duration(milliseconds: 150),
+      style: effectiveStyle,
+      child: Text(text),
+    );
+  }
+}
